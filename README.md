31b112dcb4317587f26d2504975673e98aadd1ae
Raul Piraces <raul.piraces@gmail.com>
2015-12-21 19:56:20 +0100

    Codigo comentado (casi todo). Tareas pendientes añadidas al README.md.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Seguridad
-Última práctica
+
+## Práctica 6: 
+
+### TODO:
+
+- Realizar todo tipo de pruebas para valores válidos.
+- Realizar mayores pruebas posibles para valores no válidos (de cada campo).
+- Comprobar comportamiento ante campos vacios y otros posibles valores raros.
+- Ver que lo hecho cubre todos los apartados obligatorios de la práctica.
--- src/p6/Main.java
+++ src/p6/Main.java
@@ -10,35 +10,54 @@ import org.owasp.esapi.errors.IntrusionException;
 import java.util.Scanner;
 
 /**
- * -v valida.
- * -c canonicaliza.
- * -e codifica con: SQL, HTML, URL.
+ * Práctica 6 de Seguridad Informática - Canonicalización, validación y codificación
+ *
+ * Esta clase principal, es la encargada de realizar tareas de canonicalización, validación y
+ * codificación para una serie de datos introducidos a través de un formulario.
+ * Para estas tareas, se hace uso de ESAPI (Enterprise Security API), cuya configuración ha
+ * sido establecida en el fichero "ESAPI.properties".
+ *
+ * Autores: Luis Jesús Pellicer Magallón (520256) y Raúl Piracés Alastuey (490790)
  */
 public class Main {
-    // Variables globales
+    // Variables globales finales
     public static final String VALIDAR = "-v";
     public static final String CANONICALIZAR = "-c";
     public static final String CODIFICAR = "-e";
     public static final String SQL = "SQL";
     public static final String HTML = "HTML";
     public static final String URL = "URL";
-    // Opciones elegidas por el usuario
+    // Opciones elegidas por el usuario por argumentos de invocación
     public static boolean v;
     public static boolean c;
     public static boolean e;
     public static boolean sql;
     public static boolean html;
     public static boolean url;
-    // Cadenas de texto tratadas
+    // Cadenas de texto tratadas (correspondientes al formulario tratado)
     static String nombre; static String direccion; static String dni;
     static String tipo; static String numero; static String mes; static String anio; static String cvn;
 
+    /**
+     * Metodo que valida todas las cadenas de texto recibidas a traves del formulario interactivo con el usuario.
+     * Hace uso de la clase Validator de ESAPI, para validar todas las cadenas según unas expresiones regulares,
+     * previamente establecidas en el fichero "validation.properties". Informa cuando alguna de estas cadenas falla
+     * en su validación.
+     * @return boolean true si todas las cadenas se han validado correctamente, false en otro caso.
+     */
     public static boolean validar() {
+        // Se establece el contexto de la validación (útil para logs)
         String contexto = "Validacion";
         try {
+            // Se declara un objeto Validator (ESAPI), para realizar la validación de las cadenas de texto
             Validator val = ESAPI.validator();
             boolean fallo = false;
             System.out.println("");
+            /*
+             *  Realiza la validación para todos los campos del formulario, indicando cuales de ellos fallan
+             *  (si fallan). La validación se realiza mediante las expresiones regulares declaradas en el fichero
+             *  "validation.properties".
+             */
             if(!val.isValidInput(contexto, nombre, "Name", 50, false)){
                 System.out.println("Fallo al validar el campo \"Nombre\"");
                 fallo = true;
@@ -73,13 +92,23 @@ public class Main {
             }
             return !fallo;
         } catch (IntrusionException ex){
+            // Si se detecta una "intrusión", se avisa al usuario
             System.err.println("Intrusion Exception: " + ex.getMessage());
             return false;
         }
     }
 
+    /**
+     * Método que se encarga de canonicalizar todas las cadenas introducidas en el formulario.
+     * Convierte las cadenas recibidas como entrada a su forma más simple posible, reduciendo
+     * una cadena de caracteres a su forma canónica (representación única).
+     * Esta tarea soluciona problemas de diversas representaciones de los mismos datos, que
+     * puedan dar lugar a bugs y comportamientos no deseados (así como fallos de seguridad).
+     */
     public static void canonicalizar(){
+        // Crea una instancia del objeto Encoder (ESAPI)
         Encoder enc = ESAPI.encoder();
+        // Canonicaliza todas las cadenas requeridas
         nombre = enc.canonicalize(nombre);
         direccion = enc.canonicalize(direccion);
         dni = enc.canonicalize(dni);
@@ -90,10 +119,19 @@ public class Main {
         cvn = enc.canonicalize(cvn);
     }
 
+    /**
+     * Método encargado de realizar las diferentes tareas de codificación.
+     * Es posible codificar las cadenas del formulario para SQL (MySQL), HTML y URLs.
+     * La codificación para este tipo de formatos, evita posibles errores o fallos de seguridad, al adaptar cada
+     * cadena al formato específico de cada codificación. Esto da como lugar una representación de los caracteres de
+     * la cadena única, que impide posibles fallos futuros (en su posterior utilización).
+     */
     public static void codificar(){
+        // Crea una instancia del objeto Encoder (ESAPI)
         Encoder enc = ESAPI.encoder();
         System.out.println("\n\n--- Resultados de codificación ---");
         try {
+            // Codificación de tipo SQL (si esta establecido), en concreto MySQL
             if (sql) {
                 MySQLCodec codec = new MySQLCodec(MySQLCodec.Mode.STANDARD);
                 System.out.println("-- Codificación para MySQL --");
@@ -106,6 +144,7 @@ public class Main {
                 System.out.println("Año de expiración tarjeta crédito: " + enc.encodeForSQL(codec, anio));
                 System.out.println("Código CVN tarjeta crédito: " + enc.encodeForSQL(codec, numero) + "\n");
             }
+            // Codificación de tipo HTML (si esta establecido)
             if (html) {
                 System.out.println("-- Codificación para HTML --");
                 System.out.println("Nombre: " + enc.encodeForHTML(nombre));
@@ -120,6 +159,7 @@ public class Main {
                 System.out.println("Código CVN tarjeta crédito: "
                         + enc.encodeForHTML(numero) + "\n");
             }
+            // Codificación de tipo URL (si esta establecido)
             if (url) {
                 System.out.println("-- Codificación para URL --");
                 System.out.println("Nombre: " + enc.encodeForURL(nombre));
@@ -132,11 +172,21 @@ public class Main {
                 System.out.println("Código CVN tarjeta crédito: " + enc.encodeForURL(numero) + "\n");
             }
         } catch(EncodingException ex){
+            // Si se produce cualquier error en la codificación, se notifica al usuario
             System.err.println("Error al codificar los datos: " + ex.getMessage());
         }
     }
 
+    /**
+     * Método encargado de realizar la interacción con el usuario para el relleno del formulario establecido.
+     * Acepta de uno en uno, todos los datos del formulario por la entrada estándar, de forma interactiva
+     * (aunque puede ser automatizada). Posteriormente, este mismo método, se encarga de canonicalizar, validar y
+     * codificar (en diferentes formatos) según las opciones elegidas por el usuario
+     * (contemplando posibles errores en cada tarea).
+     */
     public static void interaccion(){
+        // Se establece un método de interacción (mediante entrada estandar) con el usuario
+        // Mediante la interacción se rellenan todos los campos necesarios del formulario
         Scanner entrada = new Scanner(System.in);
         System.out.println("------ Formulario personal ------");
         System.out.print("Nombre: ");
@@ -158,31 +208,53 @@ public class Main {
         cvn = entrada.nextLine();
         entrada.close();
         System.out.println("------ Fin del formulario personal ------\n");
+        // Tareas a realizar
+        // Si esta establecido, se realiza la tarea de canonicalización
         if(c){
+
             canonicalizar();
         }
         boolean validar = true;
+        // Si esta establecido, se realiza la tarea de validación
         if(v){
             validar = validar();
         }
+        // Si esta establecido, y la validación a sido satisfactoria, se codifican los datos (según lo establecido)
         if(e && validar){
             codificar();
         } else {
+            // Si no se produce una validación correcta, se avisa al usuario
             System.out.println("\nError en validación, se omite la codificación...");
         }
     }
 
+    /**
+     * Punto de entrada del programa (método principal).
+     * Este método, se encarga principalmente de parsear los argumentos de entrada para establecer las tareas a
+     * realizar y posteriormente llamar al método encargado de ejecutarlas. Acepta todo tipo de combinaciones de
+     * argumentos válidos. Si se detecta un error en la sintaxis de los argumentos, se informa al usuario y se
+     * cancela la ejecución del programa.
+     *
+     * Ejecución: nombrePrograma [-c|-v|-e] (SQL|HTML|URL).
+     * Para la opción "-e", siempre debe de preceder el tipo de codificación (SQL, HTML o URL).
+     * @param args son los argumentos recibidos en la ejecución del programa.
+     */
     public static void main(String[] args){
+        // Comprueba el número de argumentos
         if(args.length>=0) {
+            // Itera a traves de todos los argumentos pasados, indicando las tareas a realizar por el programa
             for (int i = 0; i < args.length; i++) {
                 String entrada = args[i].trim();
+                // Reconocimiento de tipo de opción leida
                 if (entrada.equalsIgnoreCase(VALIDAR)) {
                     v = true;
                 } else if (entrada.equalsIgnoreCase(CANONICALIZAR)) {
                     c = true;
                 } else if (entrada.equalsIgnoreCase(CODIFICAR)) {
+                    // En el caso de codificación, un argumento extra debe de ser introducido
                     if (args.length > i+1) {
                         e = true;
+                        // Reconocimiento de codificación seleccionada
                         if(SQL.equalsIgnoreCase(args[i+1].trim())){
                             sql = true;
                         } else if (HTML.equalsIgnoreCase(args[i+1].trim())){
@@ -190,24 +262,28 @@ public class Main {
                         } else if (URL.equalsIgnoreCase(args[i+1].trim())){
                             url = true;
                         } else {
+                            // Codificación no correcta
                             System.err.println("Error, opción -e sin tipo correcto especificado.");
                             System.exit(1);
                         }
                         i++;
                     } else {
-                        System.err.println("Error, formato erróneo, se debe especificar un parámetro más.");
+                        // Codificación no especificada
+                        System.err.println("Error, formato erróneo, se debe especificar un argumento más.");
                         System.exit(1);
                     }
                 } else {
+                    // Opción no reconocida introducida
                     System.err.println("Error, opción inválida.");
                     System.exit(1);
                 }
             }
         } else {
-            System.err.println("Error, formato erróneo, se debe especificar un parámetro (como mínimo).");
+            // Número de argumentos inválido
+            System.err.println("Error, formato erróneo, se debe especificar un argumento (como mínimo).");
             System.exit(1);
         }
-        // Llamada al metodo principal
+        // Llamada al metodo principal de interacción y realización de tareas
         interaccion();
     }
 }
